f4f6f0204323554a491f8bce709a47a51e1a1a0c
Shreyash_2024 <newshreyashjourney@gmail.com>
2025-09-04 08:16:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const buttons = document.getElementsByClassName("exploreBtn");
 
   for (let btn of buttons) {
     btn.addEventListener("click", function() {
-      alert("Country ready to be explored!");
+      alert("Country ready for exploration!");
     });
   }
+
 
