75652774f22af1f72c19b479d66a966104736e02
Shreyash Mandlapure <shreyashmandlapure09@gmail.com>
2025-09-21 23:35:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const buttons = document.getElementsByClassName("exploreBtn");
 
   for (let btn of buttons) {
     btn.addEventListener("click", function() {
-      alert("Country ready for exploration!");
+      alert("Country ready to be explored!");
     });
   }
+
+const form = document.querySelector("form");
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting us!");
+});
+
+
+
+
+
 
 
